7deea022defdffec5e42ed4c893e90b11b56bb91
ennegineer <enne.face@gmail.com>
2022-06-07 22:16:09 -0700

    init

---
+++ Day1/dir.py
@@ -0,0 +1,17 @@
+# The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
+
+# Starting coords: (0,0)
+    # x = 0
+    # y = 0
+# Starting dir: facing North
+    # dir = North
+    
+# If facing North, L move x negative the value; R move x positive the value
+    # If L, dir = West; if R, dir = East
+# If facing East, L move y positive the value; R move y negative the value
+    # If L, dir = North; if R, dir = South
+# If facing South, L move x positive the value; R move x negative the value
+    # If L, dir = East; if R, dir = West
+# If facing West, L move y negative the value; R move y positive the value
+    # If L, dir = South; if R, dir = North
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AdventOfCode2016
-Advent 2016!
+# Advent Of Code 2016
+
+Advent 2016! Let's see if I can get through these....
+
+https://adventofcode.com for funsies!
