ee5a0c954b7bd5b9f77103787aed5fad42830d08
ennegineer <enne.face@gmail.com>
2022-07-26 20:10:30 -0700

    Part 1 working!

--- Day1/dir.py
+++ Day1/dir.py
@@ -33,13 +33,14 @@ df['Movement'] = df.Input.str[1:]
 
 # print(df.head())
 
+test = ['R5','L5','R5','R3']
 def PartOne():
     # Starting coords: (0,0)
     x = 0
     y = 0
     # Starting dir: facing North
     dir = 'North'
-
+# df['Input']
     for row in df['Input']:
     # If facing North, L move x negative the value; R move x positive the value
         # If L, dir = West; if R, dir = East
@@ -47,41 +48,66 @@ def PartOne():
             if row[0] == 'L':
                 x = x - int(row[1:])
                 dir = 'West'
-            if row[0] == 'R':
+                # print('North - L')
+                # print(x)
+                # print(y)
+            elif row[0] == 'R':
                 x = x + int(row[1:])
                 dir = 'East'
+                # print('North - R')
+                # print(x)
+                # print(y)
     # If facing East, L move y positive the value; R move y negative the value
         # If L, dir = North; if R, dir = South
-        if dir == 'East':
+        elif dir == 'East':
             if row[0] == 'L':
                 y = y + int(row[1:])
                 dir = 'North'
-            if row[0] == 'R':
+                # print('East - L')
+                # print(x)
+                # print(y)
+            elif row[0] == 'R':
                 y = y - int(row[1:])
                 dir = 'South'
+                # print('East - R')
+                # print(x)
+                # print(y)
     # If facing South, L move x positive the value; R move x negative the value
         # If L, dir = East; if R, dir = West
-        if dir == 'South':
+        elif dir == 'South':
             if row[0] == 'L':
                 x = x + int(row[1:])
                 dir = 'East'
-            if row[0] == 'R':
+                # print('South - L')
+                # print(x)
+                # print(y)
+            elif row[0] == 'R':
                 x = x - int(row[1:])
                 dir = 'West'
+                # print('South - R')
+                # print(x)
+                # print(y)
     # If facing West, L move y negative the value; R move y positive the value
         # If L, dir = South; if R, dir = North
-        if dir == 'West':
+        elif dir == 'West':
             if row[0] == 'L':
                 y = y - int(row[1:])
                 dir = 'South'
-            if row[0] == 'R':
+                # print('West - L')
+                # print(x)
+                # print(y)
+            elif row[0] == 'R':
                 y = y + int(row[1:])
                 dir = 'North'
+                # print('West - R')
+                # print(x)
+                # print(y)
 
     print(f'coordinates: ({x}, {y}) direction: {dir}')
-    
+
     answer = (abs(0 - x)) + (abs(0 - y))
     print(answer)
+    print(x+y)
 
 PartOne()
 
@@ -90,3 +116,5 @@ PartOne()
 # x2 = -491, y1 = -114
 
 # |x1 - x2| + |y1 - y2|
+
+# 605 is too high!
