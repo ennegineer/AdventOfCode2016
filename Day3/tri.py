f2d29ce5d38e3fa495cbf570220139610883df8c
ennegineer <enne.face@gmail.com>
2022-08-15 21:24:47 -0700

    Day 3 part 2 completed

--- Day3/tri.py
+++ Day3/tri.py
@@ -26,4 +26,21 @@ def PartOne():
             possible += 1
     print(possible)
 
-PartOne()
+# PartOne()
+
+def PartTwo():
+    # rows are unrelated - each set of three numbers vertically = a triangle.
+    possible = 0
+    print(triangles[0], triangles[-1])
+    for i, row in enumerate(triangles):
+        # if row is 0 OR mod3
+        if (i == 0) or (i % 3 == 0):
+            if (int(row[0]) + int(triangles[i+1][0]) > int(triangles[i+2][0])) and (int(triangles[i+1][0]) + int(triangles[i+2][0]) > int(row[0])) and (int(row[0]) + int(triangles[i+2][0]) > int(triangles[i+1][0])):
+                possible += 1
+            if (int(row[1]) + int(triangles[i+1][1]) > int(triangles[i+2][1])) and (int(triangles[i+1][1]) + int(triangles[i+2][1]) > int(row[1])) and (int(row[1]) + int(triangles[i+2][1]) > int(triangles[i+1][1])):
+                possible += 1
+            if (int(row[2]) + int(triangles[i+1][2]) > int(triangles[i+2][2])) and (int(triangles[i+1][2]) + int(triangles[i+2][2]) > int(row[2])) and (int(row[2]) + int(triangles[i+2][2]) > int(triangles[i+1][2])):
+                possible += 1
+    
+    print(possible)
+PartTwo()
